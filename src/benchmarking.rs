241f7354694fc2ecf3b04d6ee2631dc04cb05b57
SailorSnoW <snxwin@pm.me>
2022-09-19 21:17:37 +0200

    Adding benchmark implementations

--- src/benchmarking.rs
+++ src/benchmarking.rs
@@ -5,22 +5,47 @@
 
 use super::*;
 use frame_benchmarking::{
-    account, benchmarks_instance_pallet, whitelist_account, whitelisted_caller,
+    account, benchmarks, impl_benchmark_test, impl_benchmark_test_suite, whitelist_account,
+    whitelisted_caller,
 };
 use frame_support::traits::{EnsureOrigin, Get};
 use frame_system::RawOrigin as SystemOrigin;
+use rand::{thread_rng, Rng};
 use sp_runtime::traits::Bounded;
+use sp_runtime::traits::StaticLookup;
 use sp_std::prelude::*;
 
 use crate::Pallet as Artists;
 
-type BalanceOf<T> =
-    <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
-
-fn assert_last_event<T: Config, I: 'static>(generic_event: <T as Config>::Event) {
+fn assert_last_event<T: Config>(generic_event: <T as Config>::Event) {
     frame_system::Pallet::<T>::assert_last_event(generic_event.into());
 }
 
-fn assert_event<T: Config, I: 'static>(generic_event: <T as Config>::Event) {
-    frame_system::Pallet::<T>::assert_has_event(generic_event.into());
+/// Helper function that generates a random string from a given length
+fn generate_random_string(length: usize) -> String {
+    let chars: Vec<char> = "abcdefghijklmnopqrstuvwxyz".chars().collect();
+    let mut result = String::with_capacity(length);
+    let mut rng = thread_rng();
+    for _ in 0..length {
+        let x: usize = rng.gen();
+        result.push(chars[x % chars.len()])
+    }
+    result
+}
+
+benchmarks! {
+    where_clause { where T: Config }
+
+    submit_candidacy {
+        let n in 1..T::NameMaxLength::get();
+        let caller: T::AccountId = whitelisted_caller();
+        let caller_lookup = T::Lookup::unlookup(caller.clone());
+        T::Currency::make_free_balance_be(&caller, BalanceOf::<T>::max_value());
+    }: _(SystemOrigin::Signed(caller.clone()), generate_random_string(n.try_into().unwrap()).as_bytes().to_vec())
+}
+
+impl_benchmark_test_suite! {
+    Pallet,
+    crate::mock::new_test_ext(false),
+    crate::mock::Test
 }
